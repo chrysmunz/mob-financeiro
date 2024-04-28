3b82e27f0fb638a13434ce1346708d81d2deb7be
chrysmunz <chrys.munz@hotmail.com>
2024-04-28 10:46:57 -0300

    chore: :wrench: theme setup

---
+++ src/theme/index.ts
@@ -0,0 +1,8 @@
+import color from './color';
+import typography from './typography';
+
+export const theme = {
+    title: 'main',
+    color,
+    typography
+} as const;
---
+++ src/theme/typography/index.ts
@@ -0,0 +1,19 @@
+export default {
+    weight: {
+        regular: '400',
+        medium: '500',
+        semiBold: '600',
+        bold: '700',
+        extraBold: '800'
+    },
+    size: {
+        extraSmall: 10,
+        small: 12,
+        regular: 14,
+        medium: 16,
+        large: 18,
+        extraLarge: 21,
+        font24: 24,
+        font33: 33
+    }
+} as const;
